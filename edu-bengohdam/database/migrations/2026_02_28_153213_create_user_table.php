c93321e89773ba6e9bd898cda90cf8b292f4a6a4
zzlivia <geemaolivia@gmail.com>
2026-02-28 23:45:01 +0800

    fix: fix typo in user table

--- edu-bengohdam/database/migrations/2026_02_28_153213_create_user_table.php
+++ edu-bengohdam/database/migrations/2026_02_28_153213_create_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id('userID'); // primary Key
             $table->string('userName');
             $table->string('userEmail')->unique();
