22e02bbce82954e0ae1a8ab478f6f986a0c05143
zzlivia <geemaolivia@gmail.com>
2026-03-01 00:16:20 +0800

    update: add schema for lecture

--- edu-bengohdam/database/migrations/2026_02_28_153410_create_lecture_table.php
+++ edu-bengohdam/database/migrations/2026_02_28_153410_create_lecture_table.php
@@ -11,8 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // represents individual lessons within a module
         Schema::create('lecture', function (Blueprint $table) {
-            $table->id();
+            $table->id('lectID');
+            $table->foreignId('moduleID')->constrained('modules', 'moduleID')->onDelete('cascade');
+            $table->string('lectName');
+            
+            // Add ->nullable() here
+            $table->foreignId('learningMaterialID')
+                ->nullable() 
+                ->constrained('learning_materials', 'learningMaterialID')
+                ->onDelete('set null'); // Keeps the lecture even if material is deleted
+
             $table->timestamps();
         });
     }
