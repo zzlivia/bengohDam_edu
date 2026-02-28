40558f09eff79ce68ca6a974b62f6c772dd97808
zzlivia <geemaolivia@gmail.com>
2026-03-01 00:15:13 +0800

    update: add schema for module

--- edu-bengohdam/database/migrations/2026_02_28_153358_create_module_table.php
+++ edu-bengohdam/database/migrations/2026_02_28_153358_create_module_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('module', function (Blueprint $table) {
-            $table->id();
+            $table->id('moduleID'); 
+            $table->string('moduleName');
+            $table->foreignId('courseID')->constrained('courses', 'courseID')->onDelete('cascade');
+            //$table->integer('lectID');
             $table->timestamps();
         });
     }
