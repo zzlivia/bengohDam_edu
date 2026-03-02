59a7a6608db99ce9fc63ced091ba2be8122b0b1b
zzlivia <geemaolivia@gmail.com>
2026-03-02 22:46:53 +0800

    refactor: remove learningMaterialID

--- edu-bengohdam/database/migrations/2026_03_02_143626_remove_learning_material_i_d_from_lecture.php
+++ edu-bengohdam/database/migrations/2026_03_02_143626_remove_learning_material_i_d_from_lecture.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('lecture', function (Blueprint $table) {
-            $table->dropForeign(['learningMaterialID']);
             $table->dropColumn('learningMaterialID');
         });
     }
