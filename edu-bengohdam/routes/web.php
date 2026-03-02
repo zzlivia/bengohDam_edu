b97c39fbd053eb3ddc24dd1e95b2403bb4bb4141
zzlivia <geemaolivia@gmail.com>
2026-03-02 10:57:38 +0800

    fix: remove errors

--- edu-bengohdam/routes/web.php
+++ edu-bengohdam/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthenticationController;
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-});
+Route::get('/login', [AuthenticationController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
