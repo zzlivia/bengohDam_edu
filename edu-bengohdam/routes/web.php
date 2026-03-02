bd27280dde736f59831074b3296c1fcb754e5105
zzlivia <geemaolivia@gmail.com>
2026-03-02 10:48:22 +0800

    add: create route for authentication

--- edu-bengohdam/routes/web.php
+++ edu-bengohdam/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
