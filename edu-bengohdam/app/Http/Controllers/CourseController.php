aecf7981f5a22dd477544c992cbb3a6fa60af2f0
zzlivia <geemaolivia@gmail.com>
2026-03-02 13:23:14 +0800

    fix: change typo

--- edu-bengohdam/app/Http/Controllers/CourseController.php
+++ edu-bengohdam/app/Http/Controllers/CourseController.php
@@ -19,7 +19,7 @@ class CourseController extends Controller
 
         $courses = $query->paginate(5);
 
-        return view('learners.view_all_course', compact('courss'));
+        return view('learners.view_all_course', compact('courses'));
     }
 
     //show single course details
