12096cfff72f3b29b532d98dba6b216c4e4b543b
zzlivia <geemaolivia@gmail.com>
2026-03-02 11:16:39 +0800

    add: create authentication route

--- edu-bengohdam/routes/web.php
+++ edu-bengohdam/routes/web.php
@@ -5,3 +5,12 @@ use App\Http\Controllers\AuthenticationController;
 Route::get('/login', [AuthenticationController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthenticationController::class, 'login']);
 Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+
+//create learner's homepage route
+Route::middleware('auth')->group(function () {
+
+    Route::get('/homepage', function () {
+        return view('learner.homepage');
+    })->name('learner.homepage');
+
+});
