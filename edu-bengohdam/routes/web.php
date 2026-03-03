e4d9869708fdc64af5dda2e2fcd552e31c19c268
zzlivia <geemaolivia@gmail.com>
2026-03-03 13:52:26 +0800

    add: create question route

--- edu-bengohdam/routes/web.php
+++ edu-bengohdam/routes/web.php
@@ -4,16 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\CourseController;
 
-// root homepage
-Route::get('/', function () {
-    return view('learner.homepage');
-});
+//root homepage
+Route::get('/', function () {return view('learner.homepage');});
 
 //homepage
-Route::get('/homepage', function () {
-    return view('learner.homepage');
-})->name('learner.homepage');
-
+Route::get('/homepage', function () {return view('learner.homepage');})->name('learner.homepage');
 
 //authentication
 Route::get('/login', [AuthenticationController::class, 'showLogin'])->name('login');
@@ -25,11 +20,12 @@ Route::post('/register', [AuthenticationController::class, 'register']);
 Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
 
 //admin section
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware('auth:admin')->name('admin.dashboard');
+Route::get('/admin/dashboard', function () {return view('admin.dashboard');})->middleware('auth:admin')->name('admin.dashboard');
 
 //course section
 Route::get('/courses', [CourseController::class, 'index'])->name('courses.index'); //show all courses
 Route::get('/courses/{id}', [CourseController::class, 'show'])->name('courses.show'); //show a course
 Route::get('/courses/{id}/learn', [CourseController::class, 'startLearning'])->name('courses.learn'); //show page of modules
+
+//question section
+Route::get('/module/{id}/quiz', [CourseController::class, 'showQuiz'])->name('module.quiz');
