d17539516d69cc78c665901ac821f5d69176ac68
zzlivia <geemaolivia@gmail.com>
2026-03-03 17:45:53 +0800

    add: create community model

--- edu-bengohdam/app/Models/CommunityStory.php
+++ edu-bengohdam/app/Models/CommunityStory.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class CommunityStory extends Model
 {
-    //
+    protected $fillable = [
+        'community_name',
+        'title',
+        'community_story',
+        'community_image',
+        'is_active'
+    ];
 }
