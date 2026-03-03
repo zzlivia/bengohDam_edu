de89d0371e6ee9a1fe86046b536808a3b95bf2ae
zzlivia <geemaolivia@gmail.com>
2026-03-03 14:13:55 +0800

    fix: update

--- edu-bengohdam/app/Models/Mcqs.php
+++ edu-bengohdam/app/Models/Mcqs.php
@@ -18,13 +18,12 @@ class Mcqs extends Model
         return $this->belongsTo(Module::class, 'moduleID', 'moduleID');
     }
 
-    public function lecture()
-    {
-        return $this->belongsTo(Lecture::class, 'lectID', 'lectID');
-    }
-
     public function answers()
     {
         return $this->hasMany(ModuleAns::class, 'moduleQs_ID', 'moduleQs_ID');
+    }
+    public function lecture()
+    {
+        return $this->belongsTo(Lecture::class, 'lectID', 'lectID');
     }
 }
