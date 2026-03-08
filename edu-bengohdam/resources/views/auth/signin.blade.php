b116f9a0c263059177d9a67ff53e13fc8bd5e50b
zzlivia <geemaolivia@gmail.com>
2026-03-08 20:29:59 +0800

    fix: typo

--- edu-bengohdam/resources/views/auth/signin.blade.php
+++ edu-bengohdam/resources/views/auth/signin.blade.php
@@ -117,7 +117,7 @@
                     {{ session('success') }}
                 </div>
                 @endif
-                <form method="POST" action="{{ url('/login') }}">
+                <form method="POST" action="{{ route('/login') }}">
                     @csrf
 
                     <div class="mb-3">
