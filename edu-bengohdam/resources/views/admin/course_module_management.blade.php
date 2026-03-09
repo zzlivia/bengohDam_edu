85f6b53020d25094d572ff15ae060056ddc426c3
zzlivia <geemaolivia@gmail.com>
2026-03-09 23:28:22 +0800

    update: refactor

--- edu-bengohdam/resources/views/admin/course_module_management.blade.php
+++ edu-bengohdam/resources/views/admin/course_module_management.blade.php
@@ -3,10 +3,8 @@
 @section('content')
 
 <h4 class="fw-bold mb-4">Course/Module Management</h4>
-
 {{-- Summary Cards --}}
 <div class="row mb-4">
-
     <div class="col-md-3">
         <div class="card-box text-center">
             <h6>Total courses</h6>
@@ -38,7 +36,6 @@
             <small>Engaged in this week</small>
         </div>
     </div>
-
 </div>
 
 {{-- search course and add course --}}
@@ -49,42 +46,45 @@
     </a>
 </div>
 
-{{-- Table --}}
+{{-- table --}}
 <div class="card-box">
-
     <table class="table">
         <thead>
             <tr>
                 <th>No</th>
+                <th>Course Code</th>
                 <th>Course Name</th>
+                <th>Author</th>
+                <th>Availability</th>
                 <th>Action</th>
             </tr>
         </thead>
-
         <tbody>
-
         @foreach($courses as $index => $course)
         <tr>
             <td>{{ $index + 1 }}</td>
-            <td>{{ $course['name'] }}</td>
+            <td>{{ $course->courseCode }}</td>
+            <td>{{ $course->courseName }}</td>
+            <td>{{ $course->courseAuthor }}</td>
+            <td>
+                @if($course->isAvailable)
+                    <span class="badge bg-success">Available</span>
+                @else
+                    <span class="badge bg-danger">Hidden</span>
+                @endif
+            </td>
             <td>
                 <a href="#">View</a> |
-                <a href="#">Delete</a> |
                 <a href="#">Edit</a> |
-                <a href="#">Hide</a> |
-                <a href="#">Show</a>
+                <a href="#">Delete</a>
             </td>
         </tr>
         @endforeach
-
         </tbody>
-
     </table>
-
+    {{-- save change button --}}
     <div class="text-center mt-3">
         <button class="btn btn-dark">Save Changes</button>
     </div>
-
 </div>
-
 @endsection
