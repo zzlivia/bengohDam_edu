e03021094844241861db66eb35a10afa8a7098a3
zzlivia <geemaolivia@gmail.com>
2026-03-09 23:39:36 +0800

    add: edit course module

---
+++ edu-bengohdam/resources/views/admin/edit_course_module.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h3>Edit Course</h3>
+    <form method="POST" action="{{ route('admin.course.update',$course->courseID) }}">
+    @csrf
+    @method('PUT')
+        <div class="mb-3">
+            <label>Course Code</label>
+            <input type="text" name="courseCode" class="form-control" value="{{ $course->courseCode }}">
+        </div>
+
+        <div class="mb-3">
+            <label>Course Name</label>
+            <input type="text" name="courseName" class="form-control" value="{{ $course->courseName }}">
+        </div>
+
+        <div class="mb-3">
+            <label>Author</label>
+            <input type="text" name="courseAuthor" class="form-control" value="{{ $course->courseAuthor }}">
+        </div>
+
+        <button class="btn btn-success">
+        Update Course
+        </button>
+    </form>
+</div>
+@endsection
