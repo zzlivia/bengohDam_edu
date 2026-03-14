d9bea63800d29680a891934a16849268709f2f49
zzlivia <geemaolivia@gmail.com>
2026-03-14 22:10:22 +0800

    fix: modify service worker

--- edu-bengohdam/public/service-worker.js
+++ edu-bengohdam/public/service-worker.js
@@ -1,7 +1,23 @@
-self.addEventListener('install', function(event) {
-    console.log('Service Worker Installed');
+const CACHE_NAME = "bengoh-academy-cache-v1";
+
+const urlsToCache = [
+    "/",
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
