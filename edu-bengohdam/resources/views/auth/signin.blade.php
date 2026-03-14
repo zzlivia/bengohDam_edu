b4eb8bcf14c65ab43ef78c7d01956169c1bc8350
zzlivia <geemaolivia@gmail.com>
2026-03-14 14:00:53 +0800

    add: disable autofill

--- edu-bengohdam/resources/views/auth/signin.blade.php
+++ edu-bengohdam/resources/views/auth/signin.blade.php
@@ -24,7 +24,7 @@
                 </div>
             @endif
 
-            <form method="POST" action="{{ route('login') }}">
+            <form method="POST" action="{{ route('login') }}" autocomplete="off">
                 @csrf
 
                 <div class="mb-3">
