ec55bf8e1aeafabdfc075ce3dbfb90b4cc0a99a5
zzlivia <geemaolivia@gmail.com>
2026-03-17 07:18:58 +0800

    fix: comments

--- edu-bengohdam/resources/views/learner/startlearning.blade.php
+++ edu-bengohdam/resources/views/learner/startlearning.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="container-fluid mt-3">
         <div class="row">
-        <!-- Sidebar -->
+        <!-- sidebar -->
         @include('partials.course-sidebar')
-            <!-- Main Content -->
+            <!-- main content -->
             <div class="col-md-9">
                 <div class="content-section">
                     <h4 class="fw-bold">
@@ -15,17 +15,15 @@
                         <span class="text-muted">Video Placeholder</span>
                     </div>
                     @if($section)
-                        <h5>{{ $section->section_title }}</h5>
-
-                        @if($section->section_type == 'text')
-                            <p>{{ $section->section_content }}</p>
+                        <h5>{{ $section->section_title }}</h5> <!-- lecture title -->
+                        @if($section->section_type == 'text') <!-- lecture type -->
+                            <p>{{ $section->section_content }}</p> <!-- lecture content -->
                         @endif
-
-                        @if($section->section_type == 'pdf')
+                        @if($section->section_type == 'pdf') <!-- lecture pdf retrieve from storage -->
                             <iframe src="{{ asset('storage/'.$section->section_file) }}" width="100%" height="400"></iframe>
                         @endif
-
                     @endif
+                    <!-- allow user to choose to previous page or next page -->
                     <div class="d-flex justify-content-between mt-4">
                         <button class="btn btn-outline-secondary btn-nav"> Previous </button>
                         <button class="btn btn-primary btn-nav"> Next </button>
