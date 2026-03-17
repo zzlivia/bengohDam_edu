61bb2cff149a0a65b123077d3bd25c66a8343127
zzlivia <geemaolivia@gmail.com>
2026-03-18 07:49:12 +0800

    add: update blade form add image input

--- edu-bengohdam/resources/views/admin/edit_course_module.blade.php
+++ edu-bengohdam/resources/views/admin/edit_course_module.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <h3>Edit Course</h3>
-    <form method="POST" action="{{ route('admin.course.update',$course->courseID) }}">
+    <form method="POST" action="{{ route('admin.course.update',$course->courseID) }}" enctype="multipart/form-data">
     @csrf
     @method('PUT')
         <div class="mb-3">
@@ -20,7 +20,16 @@
             <label>Author</label>
             <input type="text" name="courseAuthor" class="form-control" value="{{ $course->courseAuthor }}">
         </div>
-
+        <div class="mb-3">
+            <label>Course Image</label>
+            <input type="file" name="courseImg" class="form-control">
+            
+            @if($course->courseImg)
+                <div class="mt-2">
+                    <img src="{{ asset($course->courseImg) }}" width="120">
+                </div>
+            @endif
+        </div>
         <button class="btn btn-success">Update Course</button>
         <a href="{{ route('admin.course.module') }}" class="btn btn-secondary">Back</a>
     </form>
