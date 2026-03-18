c42f7c37ae1912de874449358d1927ca72f9421d
zzlivia <geemaolivia@gmail.com>
2026-03-18 08:07:09 +0800

    fix: errors

--- edu-bengohdam/resources/views/admin/edit_course_module.blade.php
+++ edu-bengohdam/resources/views/admin/edit_course_module.blade.php
@@ -20,6 +20,35 @@
             <label>Author</label>
             <input type="text" name="courseAuthor" class="form-control" value="{{ $course->courseAuthor }}">
         </div>
+
+        <div class="mb-3">
+            <label>Course Description</label>
+            <textarea name="courseDesc" class="form-control">{{ $course->courseDesc }}</textarea>
+        </div>
+
+        <div class="mb-3">
+            <label>Course Category</label>
+            <input type="text" name="courseCategory" class="form-control" value="{{ $course->courseCategory }}">
+        </div>
+
+        <div class="mb-3">
+            <label>Course Level</label>
+            <input type="text" name="courseLevel" class="form-control" value="{{ $course->courseLevel }}">
+        </div>
+
+        <div class="mb-3">
+            <label>Course Duration</label>
+            <input type="number" name="courseDuration" class="form-control" value="{{ $course->courseDuration }}">
+        </div>
+
+        <div class="mb-3">
+            <label>Available</label>
+            <select name="isAvailable" class="form-control">
+                <option value="1" {{ $course->isAvailable ? 'selected' : '' }}>Yes</option>
+                <option value="0" {{ !$course->isAvailable ? 'selected' : '' }}>No</option>
+            </select>
+        </div>
+        
         <div class="mb-3">
             <label>Course Image</label>
             <input type="file" name="courseImg" class="form-control">
